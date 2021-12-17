fd8d07fe0ba497b8412108e2513f24703a1c331c
MohamedMedni <muhamed.medni@gmail.com>
2021-12-17 15:29:47 +0100

    your commit message here

---
+++ NetBeansProjects/ShopApps/src/duke/choice/Customer.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package duke.choice;
+
+/**
+ *
+ * @author muhamedahmed
+ */
+public class Customer {
+
+    private String name;
+    private String size;
+
+    private Clothing[] items;
+
+    public Customer(String name, int neasurement) {
+        this.name = name;
+        setSiz(neasurement);
+
+    }
+
+    public void addItems(Clothing[] someItems) {
+        items = someItems;
+    }
+
+    public Clothing[] getItems() {
+        return items;
+    }
+
+    public double getTotalClothingCost() {
+        double total = 0.0;
+        for (Clothing item : items) {
+            total = total + item.getPrice();
+        }
+        return total;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setSiz(int neasurement) {
+        switch (neasurement) {
+            case 1, 2, 3:
+                setSize("S");
+                break;
+            case 4, 5, 6:
+                setSize("M");
+                break;
+            case 7, 8, 9:
+                setSize("L");
+                break;
+
+            default:
+                setSize("XL");
+        }
+    }
+
+}
